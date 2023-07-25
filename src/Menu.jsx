db76eb3c93ce5e2c0b95ffabc3beadd47b5f3fb9
Alix Rodriguez <alix.rodriguez@jirehpro.co>
2023-07-24 23:23:40 -0400

    adaptacion a dispositivos moviles

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -43,7 +43,7 @@ function Menu() {
             <div style={backgroundStyle1} className='margarita flex flex-col items-center justify-end'>
                 <div>
                     <div className="div_botones flex  justify-center items-center">
-                        <button style={btn1} className="py-2 px-4 btn-hover rounded-full m-2">DRINKS</button>
+                        <button style={btn1} className="py-2 px-4 drinks btn-hover rounded-full m-2">DRINKS</button>
                     </div>
                 </div>
                 <p>Viva Jalisco Restaurant in Bothell is renowned for crafting the most exquisite Margaritas in town. With their blended flavors and top-quality ingredients, their Margaritas are truly a taste of perfection.</p>
@@ -54,8 +54,8 @@ function Menu() {
             <img src={img7} alt="imagen comida" />
             <h1>PARTY PLATTERS?</h1>
                 <div>
-                    <div className="div_botones flex  justify-center items-center">
-                        <button style={btn1} className="py-2 px-4 btn-hover rounded-full m-2">TOGO HERE</button>
+                    <div className="div_botones flex   justify-center items-center">
+                        <button style={btn1} className="py-2  px-4 btn-hover rounded-full m-2">TOGO HERE</button>
                     </div>
                 </div>
                
